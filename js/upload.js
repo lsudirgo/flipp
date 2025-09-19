bb5b6cc9cdba4ebe3def16ed109519c888d6f259
Ling Sudirgo <64245269+lsudirgo@users.noreply.github.com>
2025-09-19 09:58:22 +0700

    Update upload.js

--- js/upload.js
+++ js/upload.js
@@ -194,7 +194,7 @@
   
                     try {
                         showOverlay('Mengunggah PDF...');
-                        const res = await fetch('api/upload', {
+                        const res = await fetch('api/list-file/upload', {
                         method: 'POST',
                         body: formData
                         });
@@ -258,7 +258,7 @@
     // ambil daftar file dari server
 async function loadFileList() {
   try {
-    const res = await fetch('api/files');
+    const res = await fetch('api/list-file');
     const data = await res.json();
     $toclist.empty();
     if (data.success && data.files.length > 0) {
@@ -273,7 +273,7 @@ async function loadFileList() {
         item.find('.btn-delete').on('click', async () => {
           if (confirm(`Hapus file ${f}?`)) {
             try {
-              const delRes = await fetch(`api/files/${f}`, { method: 'DELETE' });
+              const delRes = await fetch(`api/list-file/${f}`, { method: 'DELETE' });
               const delData = await delRes.json();
               if (delData.success) {
                 alert('File deleted');
@@ -331,4 +331,5 @@ $(window).on('resize', () => {
   $flipbook.turn('size', $('#flipbook-wrap').width(), $('#flipbook-wrap').height());
   $flipbook.turn('page', current);
 });
+
 
